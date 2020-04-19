4a7c32e4a9d06401222f4e0f040d0d6cd24d0ec9
swarajd <swaraj.dhumne@gmail.com>
2020-04-19 13:56:41 -0400

    running the code from a file instead of from a string

--- run.py
+++ run.py
@@ -1,61 +1,74 @@
 import json
 import docker
 import requests
+import os
+import pathlib
+
+from dotenv import load_dotenv
+load_dotenv()
 
 client = docker.from_env()
 
 # malicious_cmd = "\"; sleep 10; echo \"evil"
-# malicious_cmd = """
-# import os
-# print(1234)
-# while True:
-#     pid = os.fork()
-#     print(f'pid: {pid}')
-# """
+malicious_cmd = """print(input())"""
 malicious_cmd = "print(12)"
 
 nproc_limit = docker.types.Ulimit(name="nproc", hard=3)
-# stack_size_limit = docker.types.Ulimit(name="stack", hard=1000)
+
+input_path = os.path.abspath("inputs")
+programs_path = os.path.abspath("tmp")
+
+input_mount = docker.types.Mount("/inputs/", input_path, read_only=True, type="bind")
+tmp_mount   = docker.types.Mount("/tmp/", programs_path, type="bind")
 
 class DockerTaskException(Exception):
     pass
 
-try:
-    container = client.containers.run(
-        "python:3.8-alpine",
-        ["sh", "-c", f"echo test | python -c \"{malicious_cmd}\""],
-        # ["sh", "-c", ":(){ :|:& };:"],
-        detach=True,
-        mem_limit="4m",
-        memswap_limit="4m",
-        # auto_remove=True,
-        # remove=True,
-        network_disabled=True,
-        network_mode="none",
-        pids_limit=3,
-        read_only=True,
-        ulimits=[
-            nproc_limit,
-        ]
-    )
+def execute_code(input_filename, code_filename):
+    try:
+        command = ["sh", "-c", f"python {code_filename} < {input_filename}"]
+        print(command)
+        container = client.containers.run(
+            os.getenv("IMAGE"),
+            command,
+            # ["sh", "-c", ":(){ :|:& };:"],
+            detach=True,
+            mem_limit=os.getenv("MEM_LIMIT"),
+            memswap_limit=os.getenv("MEM_LIMIT"),
+            # auto_remove=True,
+            # remove=True,
+            mounts=[
+                input_mount,
+                tmp_mount
+            ],
+            network_disabled=True,
+            network_mode="none",
+            pids_limit=int(os.getenv("PID_LIMIT")),
+            read_only=True,
+            ulimits=[
+                nproc_limit,
+            ]
+        )
 
-    exit_code = container.wait(timeout=5)
-    logs = container.logs()
-    
-    if (exit_code['StatusCode'] != 0):
-        raise DockerTaskException((exit_code, logs))
+        exit_code = container.wait(timeout=5)
+        logs = container.logs()
+        
+        if (exit_code['StatusCode'] != 0):
+            raise DockerTaskException((exit_code, logs))
 
-    print(logs)
+        print(logs)
 
-except requests.exceptions.ConnectionError as err:
-    print(f"timeout err: the container was executing for too long")
+    except requests.exceptions.ConnectionError as err:
+        print(f"timeout err: the container was executing for too long")
 
-except docker.errors.ContainerError as err:
-    print(f"some container error: {err}")
+    except docker.errors.ContainerError as err:
+        print(f"some container error: {err}")
 
-except DockerTaskException as err:
-    print(err)
+    except DockerTaskException as err:
+        print(err)
 
-finally:
-    container.stop(timeout=0)
-    container.remove()
+    finally:
+        container.stop(timeout=0)
+        container.remove()
+
+execute_code("/inputs/input.txt", "/tmp/test.py")
